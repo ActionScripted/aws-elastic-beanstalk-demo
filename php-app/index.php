dc690e7b90293ec2bbdce6d74c479f00ed9f2074
Michael Thompson <michael@clockwork360.com>
2015-08-10 13:57:14 -0400

    adjusting PHP for RDS check

--- php-app/index.php
+++ php-app/index.php
@@ -1,4 +1,9 @@
 <h1>Hello World!!!</h1>
 <h3>PHP Version <pre><?= phpversion()?></pre></h3>
 
-"<?php echo $_SERVER['RDS_HOSTNAME'] ?>"
+Hey, is RDS around?
+<?php if ($_SERVER['RDS_HOSTNAME']): ?>
+  <p><strong>YUP!</strong> RDS host name and other config are available in <code>$_SERVER</code>.</p>
+<?php else: ?>
+  <p><strong>NOPE!</strong> RDS host name and other config are NOT available in <code>$_SERVER</code>.</p>
+<?php endif; ?>
